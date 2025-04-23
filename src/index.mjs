04f0156d2b52e83ed3b0e7f9b768bfb6c630d992
Abdulsamad1109 <gbadebosamad0.gmail.com>
2025-04-23 14:01:41 +0100

    add little changes

--- src/index.mjs
+++ src/index.mjs
@@ -1,66 +1,72 @@
 import express, { request, response } from "express";
-import mongoose, {connect} from "mongoose";
+import mongoose from "mongoose";
 import MongoStore from "connect-mongo";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import session from "express-session";
-import allRoutes from './routes/index.mjs'
+import allRoutes from './routes/index.mjs';
 import passport from "passport";
-import './strategies/local-strategy.mjs'
+import './strategies/local-strategy.mjs';
 
-const app = express()
+const app = express();
 
-dotenv.config()
+dotenv.config();
 const uri = process.env.URI
 const cookieParserSecret = process.env.COOKIE_PARSER_SECRET
 const sessionSecret = process.env.SESSION_SECRET
-
-// mongoose connection
-connect(uri)
-.then(res=>{
-    console.log("connected to database")
-})
-.catch (err=>{
-    console.log("connection failed")
-})
-
-
-app.use(express.json())
-app.use(cookieParser(cookieParserSecret))
-
-app.use(session(
-    {
-        secret: sessionSecret,
-        saveUninitialized: false,
-        resave: false,
-        cookie: {
-            maxAge : 60000 * 60
-        },
-        store: MongoStore.create({
-            mongoUrl: uri
-        })
-    }
-))
-
-
-app.use(passport.initialize())
-app.use(passport.session()) 
-
-
-app.use(allRoutes) 
-
-
-app.get("/", (request,response)=>{
-    request.session.visited = true
-    response.cookie("samad","boy", {maxAge: 60000 * 10, signed: true})
-    response.status(201).send("Hello World!")
-})
-
-
-
 const PORT = process.env.PORT || 4000
 
-app.listen(PORT, ()=>{
-    console.log(`Running on port ${PORT}`);
+// mongoose connection
+mongoose.connect(uri)
+.then(res=>{
+    console.log("connected to database");
+
+
+    app.use(express.json());
+    app.use(cookieParser(cookieParserSecret));
+
+
+    app.use(session(
+        {
+            secret: sessionSecret,
+            saveUninitialized: false,
+            resave: false,
+            cookie: {
+                maxAge : 60000 * 60
+            },
+            store: MongoStore.create({
+                mongoUrl: uri
+            })
+        }
+    ));
+    
+    
+    app.use(passport.initialize());
+    app.use(passport.session());
+    
+    
+    app.use(allRoutes);
+
+
+    app.listen(PORT, ()=>{
+        console.log(`Running on port ${PORT}`);
+    });
+
+
+    app.get("/", (request,response)=>{
+        request.session.visited = true
+        response.cookie("samad","boy", {maxAge: 60000 * 60, signed: true})
+        response.status(201).send("Hello World!")
+    });
+
+    
 })
+.catch (err=>{
+    console.log("connection failed", err);
+});
+
+
+
+
+
 
